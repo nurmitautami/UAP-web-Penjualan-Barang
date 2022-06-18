f66899f82eebd4732bb19599cf55473efc83314a
Erlangga Dwi Jiwantoro <83545747+erlanggadj@users.noreply.github.com>
2022-06-18 21:43:31 +0700

    Update footer.php

--- admin/template/footer.php
+++ admin/template/footer.php
@@ -2,8 +2,7 @@
       <!--footer start-->
       <footer class="site-footer">
           <div class="text-center">
-              <?php echo date('Y');?> - Sistem Penjualan Barang Berbasis Web | 
-              By <a href="https://duamasachannel.blogspot.com" style="color:yellow;font-weight:700;" target="_blank">Dua Masa Channel</a>
+              <?php echo "PENJUALAN BARANG UAP WEB" ?> 
               <a href="#" class="go-top">
                   <i class="fa fa-angle-up"></i>
               </a>
